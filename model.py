737ad7e30f9fd5581995151145b34e26891c8d97
haoyu0831 <haoyuhe1@gmail.com>
2020-09-16 08:32:52 -0400

    finished modeling

--- model.py
+++ model.py
@@ -1,1 +1,23 @@
+import networkx as nx
+from read_file import *
+import matplotlib.pyplot as plt
 
+'''
+This function is to generate a graph with data produced by read_file.py
+'''
+
+
+def generate_network(block_ids, dest_cbgs):
+    G = nx.DiGraph()
+    # add nodes
+    G.add_nodes_from(block_ids)
+    # add edges
+    for i in dest_cbgs:
+        G.add_edge(*i, weight=dest_cbgs[i])
+
+    return G
+
+
+# file = 'data/01/01/2020-01-01-social-distancing.csv.gz'
+# G = generate_network(*read_file(file))
+# nx.draw(G)
