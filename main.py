69e841bbb8772359b30747168bf2390e57548702
haoyu0831 <haoyuhe1@gmail.com>
2020-09-16 12:03:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,17 @@ from model import *
 
 def main(threshold):
     file = 'data/01/01/2020-01-01-social-distancing.csv.gz'
-    G = generate_network(*read_file(file))
+    block_ids, dest_cbgs = read_file(file)
+    g = generate_network(block_ids, dest_cbgs)
+
+    new_dest_cbgs = dict()
+    for i in dest_cbgs.keys():
+        if dest_cbgs[i] > threshold:
+            new_dest_cbgs[i] = dest_cbgs[i] = threshold
+    percolation_g = generate_network(block_ids, new_dest_cbgs)
 
     return
+
 
 if __name__ == '__main__':
     threshold = 0
