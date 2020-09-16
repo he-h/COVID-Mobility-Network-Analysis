420c6f801239e212fb58e4f4ba426f3370cff45f
haoyu0831 <haoyuhe1@gmail.com>
2020-09-16 09:46:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import numpy as np
+from read_file import *
+from model import *
 
+
+def main(threshold):
+    file = 'data/01/01/2020-01-01-social-distancing.csv.gz'
+    G = generate_network(*read_file(file))
+
+    return
+
+if __name__ == '__main__':
+    threshold = 0
+    main(threshold)
