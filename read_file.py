e3f9204e26077afac12324ebd7067d268eed5217
haoyu0831 <haoyuhe1@gmail.com>
2020-09-21 09:51:51 -0400

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -20,23 +20,32 @@ def parse_str(tmp):
     return dest_dict
 
 
+
 '''
 This function is designed for reading and processing csv.gz file to the data frame we want especially
-origin_census_block_group and destination_cbgs from sourcegraph.com 
+origin_census_block_group and destination_cbgs from https://docs.safegraph.com/docs/social-distancing-metrics
 return with a set of block ids and a dict with key = (start, destination) value = number of people
 '''
 
 
-def read_file(path):
+def read_file(path, num):
     df = pd.read_csv(path)
     block_ids = set()
     dest_cbgs = dict()
 
     for ind in df.index:
         block = str(df['origin_census_block_group'][ind])
+        if not block.startswith(str(num)):
+            continue
         block_ids.add(block)
         dests = parse_str(df['destination_cbgs'][ind])
         for i in dests.keys():
+            if not i.startswith(str(num)):
+                continue
             dest_cbgs[(block, i)] = dests[i]
 
     return block_ids, dest_cbgs
+
+
+# file = 'data/01/01/2020-01-01-social-distancing.csv.gz'
+# print(len(read_file(file, 25)[1]))
