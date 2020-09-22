142ad5c857842785f0b8270ae453ee346174ca7c
haoyu0831 <haoyuhe1@gmail.com>
2020-09-22 11:36:28 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import numpy as np
 from read_file import *
 from model import *
+import matplotlib.pyplot as plt
 
 
-def main(threshold):
-    file = 'data/01/01/2020-01-01-social-distancing.csv.gz'
-    block_ids, dest_cbgs = read_file(file)
-    g = generate_network(block_ids, dest_cbgs)
+'''
+This function is aim to plot number of element of G and SG with changing threshold described in the paper
+'''
 
-    new_dest_cbgs = dict()
-    for i in dest_cbgs.keys():
-        if dest_cbgs[i] > threshold:
-            new_dest_cbgs[i] = dest_cbgs[i] = threshold
-    percolation_g = generate_network(block_ids, new_dest_cbgs)
+
+def plot_g_sg(file, num):
+    block_ids, dest_cbgs = read_file(file, num)
+    G = generate_network(block_ids, dest_cbgs)
+    max_w = max_weight(G)
+
+    # setting thresholds
+    thresholds = np.arange(0, max_w, 1)
+    num_g, num_sg = num_g_sg(generate_network(block_ids, dest_cbgs, thresholds))
+
+    # plotting
+    plt.xlabel('threshold')
+    plt.ylabel('size of scc')
+    plt.plot(thresholds, num_g, 'G')
+    plt.plot(thresholds, num_sg, 'SG')
+    plt.legend()
+    plt.show()
+
+
+def main(file, state_id):
+    plot_g_sg(file, state_id)
 
     return
 
 
 if __name__ == '__main__':
-    threshold = 0
-    main(threshold)
+    path = 'data/01/01/2020-01-01-social-distancing.csv.gz'
+    state_id = 25
+    main(path, state_id)
