7b7bc7f4fa88cc325bac7d362769ad0fc55a3f80
haoyu0831 <haoyuhe1@gmail.com>
2020-09-28 10:34:57 -0400

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -28,21 +28,60 @@ return with a set of block ids and a dict with key = (start, destination) value 
 '''
 
 
-def read_file(path, num):
+def read_file(path, dest_cbgs, num):
     df = pd.read_csv(path)
-    block_ids = set()
+
+    for ind in df.index:
+        block = str(df['origin_census_block_group'][ind])
+        if not block.startswith(str(num)):
+            continue
+            
+        dests = parse_str(df['destination_cbgs'][ind])
+        for i in dests.keys():
+            if not i.startswith(str(num)):
+                continue
+            dest_cbgs[(block, i)] += dests[i]
+
+    return
+
+
+'''
+this function reads all the ids and return a dict with edges with weight=0
+'''
+
+
+def read_id(path, num):
+    df = pd.read_csv(path)
+    block_ids = []
     dest_cbgs = dict()
 
     for ind in df.index:
         block = str(df['origin_census_block_group'][ind])
         if not block.startswith(str(num)):
             continue
-        block_ids.add(block)
-        dests = parse_str(df['destination_cbgs'][ind])
-        for i in dests.keys():
-            if not i.startswith(str(num)):
-                continue
-            dest_cbgs[(block, i)] = dests[i]
+        block_ids.append(block)
+
+    for i in range(len(block_ids)):
+        for j in range(len(block_ids)):
+            if i == j:
+                dest_cbgs[(block_ids[i], block_ids[j])] = 0
+
+    return block_ids, dest_cbgs
+
+
+'''
+This function basically uses the function above to read multiple file
+'''
+
+
+def read_files(paths, num):
+    block_ids, dest_cbgs = read_id(paths[0], num)
+
+    for i in paths:
+        read_file(i, dest_cbgs, num)
+
+    for i in dest_cbgs.keys():
+        dest_cbgs[i] /= 7
 
     return block_ids, dest_cbgs
 
