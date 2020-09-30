d5b5d9c7fbf4f4fce190771edae1727c8c3b2da0
haoyu0831 <haoyuhe1@gmail.com>
2020-09-30 10:23:29 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from model import *
 import matplotlib.pyplot as plt
 
 
-max_w = 30
+max_w = 20
 
 
 '''
@@ -32,38 +32,34 @@ This function is aim to plot number of element of G and SG with changing thresho
 
 
 def plot_g_sg(x, g, sg):
-    # fig, ax = plt.subplots(constrained_layout=True)
-    #
-    # ax.scatter(x, g, label='Largest SCC')
-    # ax.scatter(x, sg, label='Second largest SCC')
-    #
-    # ax.set_xlabel('threshold')
-    # ax.set_ylabel('Size')
-    # ax.legend()
 
+    # fig, ax1 = plt.subplots()
     # ax1.set_xlabel('threshold')
-    # ax1.set_ylabel('size of G scc')
-    # ax1.scatter(x, g, s=1, label='G')
+    # ax1.set_ylabel('size')
+    # ax1.plot(x, g)
     # ax1.tick_params(axis='y')
-    # ax1.legend(loc=0)
-    #
-    #
-    # ax2.set_ylabel('size of SG scc')
-
-    # ax2.scatter(x, sg, s=1, color='orange', label='SG')
+    # ax2 = ax1.twinx()
+    # ax2.plot(x, sg, color='orange')
     # ax2.tick_params(axis='y')
-    # ax2.legend(loc=0)
-    #
     # fig.tight_layout()
-    fig, ax1 = plt.subplots()
-    ax1.set_xlabel('threshold')
-    ax1.set_ylabel('size')
-    ax1.plot(x, g)
-    ax1.tick_params(axis='y')
-    ax2 = ax1.twinx()
-    ax2.plot(x, sg, color='orange')
-    ax2.tick_params(axis='y')
-    fig.tight_layout()
+    #
+    # plt.show()
+
+    figure, axis_1 = plt.subplots()
+
+    axis_1.plot(x, g, color='blue', label='largest SCC')
+    axis_1.set_xlabel('threshold')
+    axis_1.set_ylabel('size')
+
+    axis_2 = axis_1.twinx()
+    axis_2.plot(x, sg, color='orange', label='second largest SCC')
+    lines_1, labels_1 = axis_1.get_legend_handles_labels()
+    lines_2, labels_2 = axis_2.get_legend_handles_labels()
+
+    lines = lines_1 + lines_2
+    labels = labels_1 + labels_2
+
+    axis_1.legend(lines, labels, loc=0)
 
     plt.show()
 
@@ -117,7 +113,7 @@ This function is to generate file names with multiple dates
 def generate_file_name(num):
     names = []
     for i in range(1, num+1):
-        tmp = 'data/01/0'+str(i)+'/2020-01-0'+str(i)+'-social-distancing.csv.gz'
+        tmp = 'data/04/0'+str(i)+'/2020-04-0'+str(i)+'-social-distancing.csv.gz'
         names.append(tmp)
 
     return names
@@ -129,6 +125,7 @@ def main(file, state_id):
     thresholds, num_g, num_sg = calc_g_sg(G, block_ids, dest_cbgs)
     plot_g_sg(thresholds, num_g, num_sg)
     # print(calc_g_sg())
+
     return
 
 
