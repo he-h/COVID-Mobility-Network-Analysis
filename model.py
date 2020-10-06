79bb1c5999f230e6bdc392aee44607e595bfa7f3
Haoyu He <heh3@rpi.edu>
2020-10-06 17:19:49 -0400

    modified model function with threshold with parameter be graph and threshold

--- model.py
+++ model.py
@@ -1,23 +1,20 @@
 import networkx as nx
-from read_file import *
-import matplotlib.pyplot as plt
 
 '''
 This function is to generate a graph with data produced by read_file.py
 '''
 
 
-def generate_network(block_ids, dest_cbgs, thershold=0):
+def generate_network(block_ids, dest_cbgs):
     G = nx.DiGraph()
     # add nodes
     G.add_nodes_from(block_ids)
     # add edges
     for i in dest_cbgs:
-        if dest_cbgs[i] < thershold:
-            continue
         G.add_edge(*i, weight=dest_cbgs[i])
 
     return G
+
 
 '''
 This function has almost same function as above but generate a Directed Graph
@@ -29,10 +26,12 @@ def generate_d_network(block_ids, dest_cbgs, thershold=0):
     # add nodes
     G.add_nodes_from(block_ids)
     # add edges
-    for i in dest_cbgs:
-        if dest_cbgs[i] < thershold:
-            continue
-        G.add_edge(*i, weight=dest_cbgs[i])
+    for i, j in dest_cbgs.keys():
+        if (i, j) not in G.edges:
+            G.add_edge(i, j, weight=dest_cbgs[i, j])
+        else:
+            weight = dest_cbgs[i, j] + G.edges[i, j]['weight']
+            G.add_edge(i, j, weight=weight)
 
     return G
 
@@ -43,11 +42,16 @@ this function is to generate percolation step of undirected network with thresho
 
 
 def generate_network_threshold(g, threshold=0):
-    G = nx.Graph()
+    new_g = nx.Graph()
 
-    G.add_edges_from(g)
+    new_g.add_edges_from(g)
 
-    return G
+    edge_list = list(g.edges)
+    for i, j in edge_list:
+        if g.edges[i, j]['weight'] >= threshold:
+            new_g.add_edge(i, j)
+
+    return new_g
 
 
 '''
@@ -56,11 +60,17 @@ this function is to generate percolation step of directed network with threshold
 
 
 def generate_d_network_threshold(g, threshold=0):
-    G = nx.DiGraph()
+    new_g = nx.Graph()
 
-    G.add_edges_from(g)
+    new_g.add_edges_from(g)
 
-    return G
+    edge_list = list(g.edges)
+    for i, j in edge_list:
+        if g.edges[i, j]['weight'] >= threshold:
+            new_g.add_edge(i, j)
+
+    return new_g
+
 
 '''
 This function is find max weight of a graph
@@ -73,6 +83,7 @@ def max_weight(g):
         weight = g.edges[i[0], i[1]]['weight']
         if weight > m_weight:
             m_weight = weight
+
     return m_weight
 
 
