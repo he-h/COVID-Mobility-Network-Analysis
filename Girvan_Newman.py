6ecc4c0d26cb60d476995fcd9a10f8fcfa07e3ba
Haoyu He <heh3@rpi.edu>
2020-11-08 23:56:05 -0500

    add Girvan Newman algorithm

--- Girvan_Newman.py
+++ Girvan_Newman.py
@@ -1,3 +1,14 @@
-import networkx as nx
-import igraph
+from nation import *
+from igraph import *
+
+
+def network_graph(dest):
+    g = Graph()
+    for i, j in dest.keys():
+
+
+
+if __name__ == '__main__':
+    data = generate_files()
+    dest = read_files(data)
 
