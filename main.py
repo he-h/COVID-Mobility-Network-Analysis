a617c070d31fcaa4a68ad78feb8cad6997a3adeb
Haoyu He <heh3@rpi.edu>
2020-11-14 20:27:43 -0500

    finish implement interMSA

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     start = dt.date(2020, 1, 8)
     end = dt.date(2020, 9, 24)
 
-    tmp = dt.datetime(2020, 1, 15)
+    tmp = dt.date(2020, 1, 15)
 
     msa = ['1602', '1922', '3362', '4472', '5602']
     nation = Nation(tmp)
